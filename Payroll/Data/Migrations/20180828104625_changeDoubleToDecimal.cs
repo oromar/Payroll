37d7809469c34758f294d221a7a9fc45ab71cd57
DESKTOP-INVG9UQ\oroma <oromar.melo@gmail.com>
2018-08-28 08:17:48 -0300

    Fix Float Points

---
+++ Payroll/Data/Migrations/20180828104625_changeDoubleToDecimal.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Payroll.Data.Migrations
+{
+    public partial class changeDoubleToDecimal : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Exchange",
+                table: "Currency",
+                nullable: false,
+                oldClrType: typeof(double));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "Exchange",
+                table: "Currency",
+                nullable: false,
+                oldClrType: typeof(decimal));
+        }
+    }
+}
--- Payroll/Startup.cs
+++ Payroll/Startup.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Payroll.Data;
@@ -40,7 +35,9 @@ namespace Payroll
             services.AddDefaultIdentity<IdentityUser>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services
+                .AddMvc()
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
