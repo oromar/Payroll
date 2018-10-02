82c2b90e70f554c5d2e23fbbb177386909392420
oldm <oldm@dmz.org.br>
2018-10-02 10:28:00 -0300

    EmailSender

--- Payroll/Common/EmailSender.cs
+++ Payroll/Common/EmailSender.cs
@@ -11,14 +11,14 @@ namespace Payroll.Common
     {
         public Task SendEmailAsync(string email, string subject, string htmlMessage)
         {
-            var mail = new MailMessage("oromar.melo@gmail.com", email);
+            var mail = new MailMessage("payrollapp123@gmail.com", email);
             var client = new SmtpClient
             {
                 Port = 587,
                 DeliveryMethod = SmtpDeliveryMethod.Network,
                 UseDefaultCredentials = false,
                 EnableSsl = true,
-                Credentials = new System.Net.NetworkCredential("oromar.melo@gmail.com", "085471li#"),
+                Credentials = new System.Net.NetworkCredential("payrollapp123@gmail.com", "321654cba"),
                 Host = "smtp.gmail.com"
             };
             mail.Subject = subject;
