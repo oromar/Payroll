9af296acdd84c3f1ed3f87593566559a700598da
oldm <oldm@dmz.org.br>
2018-10-31 19:12:49 -0300

    Adjusts Projects CRUD

--- Payroll/Controllers/ProjectsController.cs
+++ Payroll/Controllers/ProjectsController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -16,12 +17,29 @@ namespace Payroll.Controllers
 
         public override Task<IActionResult> Index(int page = 1, string filter = "", string sort = "", string order = "ASC")
         {
-            ViewBag.Companies = Utils
-                .GetOptions(_businessObject
+            var companies = _businessObject
                 .GetDAO()
                 .GetContext()
                 .Company
-                .Where(a => !a.IsDeleted));
+                .Where(a => !a.IsDeleted);
+
+            ViewBag.Companies = Utils
+                .GetOptions(companies);
+
+            ViewBag.EmployeesByCompany = companies.AsEnumerable()
+            .Select(a => new
+            {
+                Key = a.Id,
+                Value = _businessObject
+                .GetDAO()
+                .GetContext()
+                .Employee
+                .Where(b => !b.IsDeleted)
+                .Where(c => c.CompanyId == a.Id)
+                .ToList()
+            })
+            .ToDictionary(t => t.Key, t => t.Value);
+
 
             return base.Index(page, filter, sort, order);
         }
